0783619d373da8fdafb1b65b856e5d4f04f6407b
Azka Azharan <azka.azharan@gmail.com>
2025-01-28 17:17:45 +0700

    feat: initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./src/config/db');
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.json({ message: 'API is Running' });
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running in port ${PORT}`);
+});
